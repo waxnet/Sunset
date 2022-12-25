2a29140cbd61919444f90782d6a63c66759faadc
b8ff <88239635+b8ff@users.noreply.github.com>
2022-12-25 19:24:21 +0100

    update
    switched from os to shutil

--- Sunset/utilities/assets.py
+++ Sunset/utilities/assets.py
@@ -1,10 +1,10 @@
 from datetime import datetime
+import shutil
 import json
-import os
 
 date = datetime.now()
 day, month, year = date.day, date.month, date.year
-console_x, console_y = os.get_terminal_size()
+console_columns = shutil.get_terminal_size().columns
 
 def get_banner(banners_file):
     loaded_banners = json.load(open(banners_file, encoding="utf-8"))
@@ -14,6 +14,6 @@ def get_banner(banners_file):
     elif day == 31 and month == 10: parts = loaded_banners["halloween"].splitlines()
     else: parts = loaded_banners["default"].splitlines()
 
-    for part in parts: banner = banner + part.center(console_x) + "\n"
+    for part in parts: banner = banner + part.center(console_columns) + "\n"
 
     return banner
