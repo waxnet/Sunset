5b198d104079e16e11bf1ecddb4259ab1ef0837d
b8ff <88239635+b8ff@users.noreply.github.com>
2022-12-25 16:09:18 +0100

    .

--- Sunset/requirements.cmd
+++
@@ -1,4 +0,0 @@
-pip install selenium
-pip install colorama
-pip install requests
-pip install pywin32
--- Sunset/utilities/cmd.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def do(commands):
-    for command in commands:
-        os.system(command)
