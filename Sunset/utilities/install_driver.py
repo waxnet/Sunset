b8b086352680ab92535bbaf2d3ddfc205d477a7a
b8ff <88239635+b8ff@users.noreply.github.com>
2023-02-21 14:30:16 +0100

    fixed chrome driver version error

--- Sunset/utilities/install_driver.py
+++ Sunset/utilities/install_driver.py
@@ -2,13 +2,17 @@ import requests
 import zipfile
 import os
 
-def run():
+def run(driver_version):
     try:
         drivers_folder = os.getenv("appdata") + "/Sunset/drivers"
-        if os.path.exists(f"{drivers_folder}/driver.exe"):
+        for file in os.listdir(drivers_folder):
+            if file.replace(".exe", "") != driver_version:
+                os.remove(f"{drivers_folder}/{file}")
+        
+        if os.path.exists(f"{drivers_folder}/{driver_version}.exe"):
             return True
 
-        request = requests.get("https://chromedriver.storage.googleapis.com/108.0.5359.71/chromedriver_win32.zip")
+        request = requests.get(f"https://chromedriver.storage.googleapis.com/{driver_version}/chromedriver_win32.zip")
         if not request.ok:
             return False
         
@@ -20,6 +24,6 @@ def run():
                 zipped_driver.extractall(drivers_folder)
         os.remove(zipped_driver_directory)
         if os.path.exists(f"{drivers_folder}/LICENSE.chromedriver"): os.remove(f"{drivers_folder}/LICENSE.chromedriver")
-        os.rename(f"{drivers_folder}/chromedriver.exe", f"{drivers_folder}/driver.exe")
+        os.rename(f"{drivers_folder}/chromedriver.exe", f"{drivers_folder}/{driver_version}.exe")
         return True
     except: return False
